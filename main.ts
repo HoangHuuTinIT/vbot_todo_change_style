525f5a16fcce85d5a791b6afe441b50ddfbf814f
HoangHuuTinIT <tin03bk@gmail.com>
2025-11-19 09:46:31 +0700

    second commit

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 import App from './App'
 import * as Pinia from 'pinia'
+import { createI18n } from 'vue-i18n'
+import messages from './locale/index'
 // #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
@@ -16,9 +18,20 @@ import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
   app.use(Pinia.createPinia())
+  const i18n = createI18n({
+      locale: 'vi', // Ngôn ngữ mặc định
+      messages,
+      legacy: false, // Dùng Composition API
+      globalInjection: true, // Cho phép dùng $t
+    })
+    app.use(i18n)
   return {
     app,
-	Pinia
+	Pinia,
+	i18n
   }
 }
+import en from './en.json'
+import vi from './vi.json'
+export default { en, vi }
 // #endif
